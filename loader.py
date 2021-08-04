0ef11590a4b25f79f8d5c461a3819de7e168c527
Jianning Li <jianning.li@student.tugraz.at>
2021-08-04 16:29:27 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ import random
 import nrrd
 
 
-
+# Data loader for patch-wise shape completion and patch-wise voxel rearrangement
 
 
 
@@ -69,43 +69,5 @@ def load_random_test_pair(list1,idx):
     return test_patch, h1, data.shape[2]
 
 
-#**************************** for evaluation ********************#
-
-def make_random_evaluate_patch(data):
-    data_list=[]
-    for x in range(4):
-        for y in range(4):
-            temp_data=np.expand_dims(np.expand_dims(data[x*128:(x+1)*128,y*128:(y+1)*128,:],axis=0),axis=4)
-            data_list.append(temp_data)
-    return np.array(data_list)
 
 
-
-
-def load_random_evaluate_pair(list1,list2,list3,idx):
-   # generate a batch of paired patches for evaluation
-    data,h1=nrrd.read(list1[idx])
-    print('data',list1[idx])
-    label,h2=nrrd.read(list2[idx])
-    print('label',list2[idx])
-
-    implant,h3=nrrd.read(list3[idx])
-    print('implant',list3[idx])
-
-    #data=pre_processing(data)
-    #label=pre_processing(label)
-    #implant=pre_processing(implant)    
-
-    z=128
-    # 512*512*128
-    data_defected=data[:,:,data.shape[2]-z:data.shape[2]]
-    label_=label[:,:,data.shape[2]-z:data.shape[2]]
-    implant=implant[:,:,data.shape[2]-z:data.shape[2]]
-
-    data_remain=data[:,:,0:data.shape[2]-z]
-
-    defected_patch=make_random_evaluate_patch(data_defected)
-    label_=np.expand_dims(np.expand_dims(label_,axis=0),axis=4)
-    implant=np.expand_dims(np.expand_dims(implant,axis=0),axis=4)
-
-    return defected_patch,label_,h2,data_remain,data.shape[2],data_defected,implant
